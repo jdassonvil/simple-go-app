e02ebe10bca1a66d27ed9100b9da2978f118961e
Jerome Dassonville <dassonville.jerome@gmail.com>
2023-01-27 15:16:20 +0100

    feat(cache): add redis cache

--- app.go
+++ app.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 
+	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 )
 
@@ -32,32 +35,36 @@ func generateToken(username string) string {
 	return sha
 }
 
-/*
-   rdb := redis.NewClient(&redis.Options{
-       Addr:     "localhost:6379",
-       Password: "", // no password set
-       DB:       0,  // use default DB
-   })
+var ctx = context.Background()
 
-   err := rdb.Set(ctx, "key", "value", 0).Err()
-   if err != nil {
-       panic(err)
-   }
+func getOrGeneratToken(username string) string {
+	redisEndpoint := fmt.Sprintf("%s:%s", os.Getenv("REDIS_ADDRESS"), os.Getenv("REDIS_PORT"))
+	log.Printf("Redis address %s", redisEndpoint)
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     redisEndpoint,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
 
-   val, err := rdb.Get(ctx, "key").Result()
-   if err != nil {
-       panic(err)
-   }
-*/
+	token, err := rdb.Get(username).Result()
+	if err != nil {
+		log.Printf("No token in cache for %s", username)
+		newToken := generateToken(username)
+		err := rdb.Set(username, newToken, 60*time.Second).Err()
+		if err != nil {
+			panic(err)
+		}
+		return newToken
+	}
 
-/*func getOrGeneratToken(username string) string {
-
-}*/
+	log.Printf("Using cache for %s", username)
+	return token
+}
 
 func tokenHandler(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	username := params["username"]
-	token := generateToken(username)
+	token := getOrGeneratToken(username)
 	fmt.Fprintf(w, token)
 	// Generate token
 }
